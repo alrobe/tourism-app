4bb23086c02268a86b3700340163ee8c369ebbf7
Alfredo Robles Bernabe <alfredo.robles@fundacion-jala.org>
2018-09-28 21:17:55 -0400

    initial commit

---
+++ src/main/java/edu/umss/fcyt/tourismapp/TourismAppApplication.java
@@ -0,0 +1,12 @@
+package edu.umss.fcyt.tourismapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismAppApplication.class, args);
+	}
+}
