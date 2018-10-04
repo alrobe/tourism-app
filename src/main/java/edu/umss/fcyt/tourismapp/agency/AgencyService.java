3e8a89937f16073701943e84674a39ddd07f7604
Alfredo.RObles <alf.asper@gmail.com>
2018-10-04 00:00:07 -0400

    creacion del rest api para crear una agencia

---
+++ src/main/java/edu/umss/fcyt/tourismapp/agency/AgencyService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.umss.fcyt.tourismapp.agency;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Alfredo Robles
+ */
+@Service
+public class AgencyService {
+
+    @Autowired
+    private AgencyRepository agencyRepository;
+
+    public Agency createAgency(Agency newAgency) {
+        return agencyRepository.save(newAgency);
+        
+    }
+}
