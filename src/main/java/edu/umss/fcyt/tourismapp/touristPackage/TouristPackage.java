bd657be5f1417a1c822799927fd3662fdf77123a
gus_xen_12 <gus.xen.12@gmail.com>
2018-10-10 21:04:37 -0400

    corrección de atributos y constructor en paquete

--- src/main/java/edu/umss/fcyt/tourismapp/touristPackage/TouristPackage.java
+++ src/main/java/edu/umss/fcyt/tourismapp/touristPackage/TouristPackage.java
@@ -1,43 +1,60 @@
-package edu.umss.fcyt.tourismapp.touristPackage;
+package edu.umss.fcyt.tourismapp.touristpackage;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
 public class TouristPackage
 {
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "package_id")
+    private Long packageId;
     @NotBlank
-    private int minimumGroup;
+    @Column(name = "minimun_group")
+    private int minimunGroup;
     @NotBlank
+    @Column(name = "category_id")
     private int categoryId;
     @Size(min=3, max=75)
     private String name;
-    @NotEmpty
+    @NotNull
     private String description;
     @NotBlank
-    private String keyword;
+    private String keywords;
     @NotBlank
     private Double inversion;
 
+    public TouristPackage(Long packageId, int minimunGroup, int category_id, String name, String description, String keywords, Double inversion){
+        this.packageId = packageId;
+        this.minimunGroup = minimunGroup;
+        this.categoryId = category_id;
+        this.name = name;
+        this.description = description;
+        this.keywords = keywords;
+        this.inversion = inversion;
+    }
+
+    public TouristPackage(){
+    }
+
     public Long getId() {
-        return id;
+        return packageId;
     }
 
-    public void setId(Long id)
+    public void setId(Long packageId)
     {
-        this.id = id;
+        this.packageId = packageId;
     }
 
-    public int getMinimumGroup() {
-        return minimumGroup;
+    public int getMinimunGroup() {
+        return minimunGroup;
     }
 
-    public void setMinimumGroup(int minimumGroup) {
-        this.minimumGroup = minimumGroup;
+    public void setMinimumGroup(int minimunGroup) {
+        this.minimunGroup = minimunGroup;
     }
 
     public int getCategoryId() {
@@ -67,12 +84,12 @@ public class TouristPackage
     }
 
     public String getKeyword() {
-        return keyword;
+        return keywords;
     }
 
-    public void setKeyword(String keyword)
+    public void setKeyword(String keywords)
     {
-        this.keyword = keyword;
+        this.keywords = keywords;
     }
 
     public Double getInversion() {
