f8f775265c45285d0566ce7edc4fd6937d496640
Alfredo.RObles <alf.asper@gmail.com>
2018-10-23 11:00:02 -0400

    registrar agencias solucionado

--- src/main/java/edu/umss/fcyt/tourismapp/agency/AgencyController.java
+++ src/main/java/edu/umss/fcyt/tourismapp/agency/AgencyController.java
@@ -9,6 +9,7 @@ import edu.umss.fcyt.tourismapp.person.Person;
 import edu.umss.fcyt.tourismapp.person.PersonService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Alfredo Robles
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/agency")
 public class AgencyController {
 
