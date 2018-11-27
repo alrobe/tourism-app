62480e92908fa42f1b6421eecfc461796918de0f
sasor <s.leamsi99@gmail.com>
2018-11-26 20:54:07 -0500

    CrossOrigin in ItinerarioController

--- src/main/java/edu/umss/fcyt/tourismapp/itinerario/ItinerarioController.java
+++ src/main/java/edu/umss/fcyt/tourismapp/itinerario/ItinerarioController.java
@@ -1,10 +1,12 @@
 package edu.umss.fcyt.tourismapp.itinerario;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class ItinerarioController {
     @Autowired
